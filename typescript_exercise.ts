4fbc2e5d3ef51a3ead3304328511435915433e89
Lars <lars@recap.dk>
2025-09-02 19:38:20 +0200

    Initial commit

---
+++ typescript_exercise.ts
@@ -0,0 +1,88 @@
+
+
+//Exercise 1:
+// declare types above arrow functions
+type IDType = number | string;
+
+const showID = (personalId: IDType) => {
+    return `Your ID is: ${personalId}`;
+};
+
+console.log(showID("lars123"));
+console.log(showID(123))
+
+
+
+//Exercize 2:
+
+type fruit = "apple" | "banana" | "orange";
+
+const eatFruit = (fruit: fruit) => {
+    console.log("You at and " + fruit)
+};
+
+eatFruit("apple");
+eatFruit("orange");
+
+
+//const typeFruit = (apple: string | banana: string): string => {
+ //   return `You ate an ${fruit}`;
+//};
+
+
+//Exercise 3:
+
+type result = true | false;
+
+const printResult = (result: result) => {
+
+    if (result === true) {
+        console.log("Pass");
+    }
+    else { console.log("Fail");
+    };
+};
+
+    printResult(true);
+
+
+    //Exercise 4:
+
+    FIIIIIIIX BOOOOOOKTYPE
+    
+    interface Book {
+        title: string;
+        pages: number;
+        }   
+
+        type SchoolTeacher = Teacher & Employee;
+
+const describeBook = (schoolTeacher: SchoolTeacher) => {
+console.log(`The book ${bookType.name} has ${schoolTeacher.subject}`);}
+
+printTeacherInfo({
+    name: "Michiel", subject: "Backend development", email: "m@m.se", id: 123 
+});
+  
+
+    //Exercise 5:
+
+    interface Teacher {
+        name: string;
+        subject: string;
+        }
+
+    interface Employee {
+        email: string;
+        id: number;
+        }
+
+
+type SchoolTeacher = Teacher & Employee;
+
+const printTeacherInfo = (schoolTeacher: SchoolTeacher) => {
+console.log(`Name: ${schoolTeacher.name} subject: ${schoolTeacher.subject} email: ${schoolTeacher.email} id: ${schoolTeacher.id}`);}
+
+printTeacherInfo({
+    name: "Michiel", subject: "Backend development", email: "m@m.se", id: 123 
+});
